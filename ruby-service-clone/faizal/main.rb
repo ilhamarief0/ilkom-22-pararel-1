a05d335c3df904f456d4c2469c67f677e1d152bd
FaizalMuhammad <22muh.izal@gmail.com>
2024-10-18 18:59:25 +0800

    read by ruby

--- ruby-service-clone/faizal/main.rb
+++ ruby-service-clone/faizal/main.rb
@@ -5,3 +5,16 @@ posts = [{id: 1, title: 'Hello', body: 'Hello World'}, {id: 2, title: 'Goodbye',
 get '/' do
     return posts.to_json
 end
+
+get '/posts/:id' do
+    id = params[:id].to_i
+    return posts.find { |post| post[:id] == id }.to_json
+    end
+
+post '/posts' do
+    body = getBody(request)
+
+    new__post = {id: posts.length, title: body['title'], body: body['body']}
+    posts.push(new__post)
+    return new__post.to_json
+end
