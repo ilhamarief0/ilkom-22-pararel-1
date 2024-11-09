e19ea806cd447dc93c87c669ce181d6ed2057ae2
alvionitasihidi <zahratunnisa1505@gmail.com>
2024-11-09 21:39:30 +0800

    create ruby

---
+++ ruby-service-clone/vio/main.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'json'
+
+# CREATE: Tambah item baru
+post '/items' do
+    data = JSON.parse(request.body.read)
+    item = Item.new(name: data['name'], quantity: data['quantity'])
+    if item.save
+      item.to_json
+    else
+      halt 422, item.errors.full_messages.to_json
+    end
+  end
